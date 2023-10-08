ce81ee849493d4e8ffc1eade972cdeb9ab1c4758
Mani Sankar Chintagunti <114079126+Mindslayer001@users.noreply.github.com>
2023-10-08 12:45:40 -0400

    Create insert-in-a-sorted-list.cpp

---
+++ Easy/Insert in a Sorted List/insert-in-a-sorted-list.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+}
+
+// } Driver Code Ends
+/*
+structure of the node of the list is as
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* sortedInsert(Node* head1, int key) {
+        Node* x = new Node(key);
+        
+        if (head1 == nullptr || head1->data >= key) {
+            x->next = head1;
+            return x;
+        }
+        
+        Node* node = head1;
+        while (node->next != nullptr) {
+            if (node->data < key && node->next->data >= key) {
+                x->next = node->next;
+                node->next = x;
+                return head1;
+            }
+            node = node->next;
+        }
+        node->next = x;
+        
+        return head1;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+        
+		int data;
+		cin>>data;
+		
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		
+		int k;
+		cin>>k;
+		Solution obj;
+		head = obj.sortedInsert(head,k);
+		printList(head); 
+	}
+	return 0; 
+} 
+
+// } Driver Code Ends
